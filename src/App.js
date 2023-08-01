b7bfbc930e431efa2dba605f61da9c049259f220
InnaMcGrane <innamcgrane@gmail.com>
2023-08-01 13:58:45 +0100

    my commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { data } from "./data";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [gifts, setGifts] = useState(0);
+  const{id,gift,image}=data[gifts];
+
+  const previousGift =()=>{
+    setGifts((gifts =>{
+      gifts--;
+      if(gifts < 0 ){
+        return data.length - 1;
+      }
+      return gifts;
+    }))
+
+}
+const nextGift =()=>{
+  setGifts((gifts =>{
+    gifts++;
+    if(gifts > data.length-1){
+      gifts = 0;
+    }
+    return gifts;
+  }))
+}
+
+ 
+  return( <div>
+    <div className="container">
+      <h1> My wishing list</h1>
     </div>
-  );
+
+     <div className="container">
+      <img src={image} width="500px"alt="laptop"/>
+     </div>
+
+
+     <div className="container">
+      <h2>{id} - {gift}</h2>
+    </div>
+    <div className="btn container">
+      <button onClick={previousGift}> Previous</button>
+        <button onClick={nextGift}> Next</button>
+    </div>
+
+ 
+
+  </div>)
+    
+  
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,55 @@
+export const data = [
+  {
+    id: 1,
+
+    gift: "Laptop",
+
+    image:
+      "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGxhcHRvcHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    id: 2,
+
+    gift: "AirPods",
+
+    image:
+      "https://images.unsplash.com/photo-1580477371194-4593e3c7c6cf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fGFpcnBvZHN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    id: 3,
+
+    gift: "Holiday",
+
+    image:
+      "https://images.unsplash.com/photo-1541417904950-b855846fe074?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fE1hbGRpdmVzfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    id: 4,
+
+    gift: "Car",
+
+    image:
+      "https://images.unsplash.com/photo-1503376780353-7e6692767b70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Y2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    id: 5,
+
+    gift: "New House",
+
+    image:
+      "https://images.unsplash.com/photo-1600585154084-4e5fe7c39198?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGhvdXNlJTIwaW50ZXJpb3J8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+  },
+
+  {
+    id: 6,
+
+    gift: "Shopping in Italy",
+
+    image:
+      "https://images.unsplash.com/photo-1689805047033-8e8bad2d99c7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8c2hvcHBpbmclMjBpbiUyMGl0YWx5fGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+  },
+];
